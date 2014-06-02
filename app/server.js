adf67c8a00bd73ca0bae7061b28ee04a4c224c90
Joseph Ilagan <joben.ilagan@gmail.com>
2014-06-02 19:15:19 +0800

    Added runserver.sh Modified server.js

--- app/server.js
+++ app/server.js
@@ -3,4 +3,4 @@ http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
     res.end('Hello World\n');
 }).listen(3000,'0.0.0.0');
-console.log('Server listening on port 3000/');
+console.log('Server listening on port 3000. Ctrl-C to quit./');
